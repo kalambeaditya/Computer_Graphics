826ceb779ac01258b27a6d1e401d8603e9728d8b
Aditya Kalambe <76927885+kalambeaditya@users.noreply.github.com>
2023-01-02 22:36:34 +0530

    Add files via upload

---
+++ LINE_C~1.C
@@ -0,0 +1,22 @@
+#include<graphics.h>  
+#include<conio.h>  
+void main()  
+{  
+    int gd=DETECT,gm;  
+    initgraph (&gd,&gm,"c:\\tc\\bgi");  
+    setbkcolor(BLACK);
+    printf("\t\t\t\n\nLINE");
+    line(50,40,190,40);
+    printf("\t\t\n\n\n\nRECTANGLE");
+    rectangle(125,115,215,165);
+    printf("\t\t\t\n\n\n\n\n\n\nARC");
+    arc(120,200,180,0,30);
+    printf("\t\n\n\n\nCIRCLE");
+    circle(120,270,30);
+    printf("\t\n\n\n\nECLIPSE");
+    ellipse(120,350,0,360,30,20);
+   printf("\t\n\n\n\nPIXEL");
+    putpixel(150,150,RED);
+
+    getch();  
+}  
